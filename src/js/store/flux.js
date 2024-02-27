f5288fcbfdac17602205c5a7c58ed69e73abd09f
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2024-02-27 20:35:09 +0000

    add people

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,20 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: []
+
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getPeople: () => {
+				fetch("https://www.swapi.tech/api/people")
+				.then(res => res.json())
+				.then(data => setStore({people: data.results}))
+				.catch(err => console.error(err))
 			}
+		
 		}
 	};
+	
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	useEffect(() => { actions.getPeople() }, [])
+
+	console.log(store.people);
+
+
+	return (
+
+		<div className="card-group overflow-scroll">
+
+		{store.people.map((item, index) => ( 
+		<div className="card" key={index}>
+		  <img src="..." className="card-img-top" alt="..."/>
+		  <div className="card-body">
+			<h5 className="card-title">{item.name}</h5>
+			<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+		))};
+
+		{/* <div className="card">
+		  <img src="..." className="card-img-top" alt="..."/>
+		  <div className="card-body">
+			<h5 className="card-title">Card title</h5>
+			<p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+			<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+
+		<div className="card">
+		  <img src="..." className="card-img-top" alt="..."/>
+		  <div className="card-body">
+			<h5 className="card-title">Card title</h5>
+			<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+			<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div> */}
+
+	  </div>
+	  
+
+
+	)
+
+
+};
+
+		
+
+
+
